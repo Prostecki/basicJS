50515405219a74e07dab380c0baae9494dfe8a72
Mark Taratynov <mark.taratynov@gmail.com>
2023-03-08 13:28:57 +0100

    replace2

--- main.js
+++ main.js
@@ -1,43 +1,20 @@
-const nouns = ['Яблоко', 'Дом', 'Стол', 'Кот', 'Солнце', 'Чашка', 'Машина', 'Книга', 'Ручка', 'Ключ'];
+let aa = "{прил} {собс} {глаг} {прил} {сущ} и затем {глаг} {сущ}";
 
-const adjectives = ['Быстрый', 'Красивый', 'Умный', 'Сильный', 'Надежный', 'Странный', 'Счастливый', 'Талантливый', 'Безопасный', 'Безумный'];
+let adjectives = ["красивый", "веселый", "умный", "милый", "смешной", "добрый", "великолепный", "любопытный", "сильный", "умелый"];
+let nouns = ["кот", "дом", "человек", "автомобиль", "город", "магазин", "стол", "солнце", "книга", "компьютер"];
+let verbs = ["бежать", "прыгать", "петь", "играть", "читать", "писать", "рисовать", "разговаривать", "учиться", "работать"];
+let properNouns = ["Петя", "Катя", "Вася", "Маша", "Аня", "Саша", "Иван", "Лена", "Артем", "Света"];
 
-const adverbs = ['Быстро', 'Тихо', 'Осторожно', 'Плавно', 'Быстрее', 'Здесь', 'Там', 'Сейчас', 'Всегда', 'Иногда'];
+function fillTemplate(template) {
+  // Заменяем шаблонные подстроки на соответствующие значения из массивов
+  let filled = template.replace(/\{прил\}/g, () => adjectives[Math.floor(Math.random() * adjectives.length)])
+                      .replace(/\{собс\}/g, () => properNouns[Math.floor(Math.random() * properNouns.length)])
+                      .replace(/\{глаг\}/g, () => verbs[Math.floor(Math.random() * verbs.length)])
+                      .replace(/\{сущ\}/g, () => nouns[Math.floor(Math.random() * nouns.length)]);
+  
+  return filled;
+}
 
-const names = ['Анна', 'Дмитрий', 'Екатерина', 'Иван', 'Ксения', 'Максим', 'Николай', 'Ольга', 'Павел', 'Светлана'];
-
-const verbs = ['Бежать', 'Смотреть', 'Говорить', 'Слушать', 'Петь', 'Читать', 'Рисовать', 'Учить', 'Думать', 'Играть'];
-
-
-let randomNouns = Math.floor(Math.random() * nouns.length);
-let randomAdj = Math.floor(Math.random() * adjectives.length);
-let randomAdverbs = Math.floor(Math.random() * adverbs.length);
-let randomNames = Math.floor(Math.random() * names.length);
-let randomVerbs = Math.floor(Math.random() * verbs.length);
-
-
-// console.log(nouns[randomNouns]);
-
-let adj = adjectives[randomAdj];
-let smthName = names[randomNames];
-let vrbs = verbs[randomVerbs];
-let smthNouns = nouns[randomNouns];
-
-
-// let tmpl = `${adj} ${smthName} ${vrbs} ${adj} ${smthName} и затем ${vrbs} ${smthName}`;
-let aa =  `{прил} {собс} {глаг} {прил} {сущ} и затем {глаг} {сущ}`;
-
-
-
-// console.log(tmpl);
-
-
-// let aa =  `${adj} ${smthName} ${verb} ${adj} ${nouns} и затем ${verb} ${nouns}`;
-
-console.log(aa.replace("{прил}", adj)
-              .replace("{собс}", smthName)
-              .replace("{глаг}", vrbs)
-              .replace("{прил}", adj)
-              .replace("{сущ}", smthNouns)
-              .replace("{глаг}", vrbs)
-              .replace("{сущ}", smthNouns));
+// Пример вызова функции
+let result = fillTemplate(aa);
+console.log(result);
